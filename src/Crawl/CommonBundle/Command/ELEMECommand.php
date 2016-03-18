fccfc2b837c1e4b691b5a64ebc4e81550df88149
marquis <511680713@qq.com>
2016-03-18 10:23:00 +0800

    update elem. command

--- src/Crawl/CommonBundle/Command/ELEMECommand.php
+++ src/Crawl/CommonBundle/Command/ELEMECommand.php
@@ -42,98 +42,12 @@ class ELEMECommand extends ContainerAwareCommand
         ]);
 
         $curlHelper = $this->getContainer()->get('crawl_common.helper.curl');
+        $takeoutHelper = $this->getContainer()->get('crawl_common.helper.takeout');
 
-        $allData = $this->getAllData($baseUrl, $place, $curlHelper);
+        $allData = $takeoutHelper->getAllData($baseUrl, $place, $curlHelper);
         $data = ['place' => $place];
-        $this->getRestaurants($allData, $data);
-        $this->findFoots($data);
-    }
-
-    /**
-     * @param $baseUrl
-     * @param $place
-     * @param int $offset
-     * @param \Crawl\CommonBundle\Helper\CurlHelper $curlHelper
-     * @return array|null
-     */
-    public function getAllData($baseUrl, $place, $curlHelper, $offset = 0)
-    {
-        $data = [];
-        while (1) {
-            $url = $baseUrl . '?geohash=' . $place . '&offset=' . $offset . '&type=geohash';
-            if ($curlHelper->curlByUrl($url) != '[]') {
-                // 解析开始
-                $dom = new ParserDom($curlHelper->curlByUrl($url));
-                $stringToArrayDom = json_decode($dom->getPlainText());
-                foreach ($stringToArrayDom as $value) {
-                    array_push($data, $value);
-                }
-                $offset = $offset + 24;
-            } else {
-                break;
-            }
-        }
-        return $data;
-    }
-
-    /**
-     * @param $dom
-     * @param array $data
-     */
-    public function getRestaurants($dom, array &$data)
-    {
-        $data['restaurants'] = [];
-        foreach ($dom as $value) {
-            array_push($data['restaurants'], array(
-                'id' => $value->id,
-                'name' => $value->name,
-                'rating' => $value->rating,
-                'latitude' => $value->latitude,
-                'longitude' => $value->longitude,
-                'address' => $value->address,
-                'foods' => null
-            ));
-        }
-    }
-
-    /**
-     * @param array $data
-     */
-    public function findFoots(array &$data)
-    {
-        foreach ($data['restaurants'] as $k => $v) {
-            $id = $v['id'];
-            $curlHelper = $this->getContainer()->get('crawl_common.helper.curl');
-            $url = 'https://www.ele.me/restapi/v4/restaurants/' . $id . '/mutimenu';
-            $dom = new ParserDom($curlHelper->curlByUrl($url));
-            $stringToArrayDom = json_decode($dom->getPlainText());
-            $this->getFoots($stringToArrayDom, $data['restaurants'][$k]);
-        }
-        $data['restaurants'] = [];
-    }
-
-    /**
-     * @param $foods
-     * @param $data
-     */
-    public function getFoots($foods, &$data)
-    {
-        $data['foods'] = [];
-        foreach ($foods as $value) {
-            if (isset($value->id)) {
-                foreach ($value->foods as $foods) {
-                    var_dump($foods);
-                    if ($foods->specfoods[0]->price >= 1) {
-                        array_push($data['foods'], array(
-                            'name' => $foods->specfoods[0]->name,
-                            'rating' => $foods->rating,
-                            'monthSales' => $foods->month_sales,
-                            'price' => $foods->specfoods[0]->price
-                        ));
-                    }
-                }
-            }
-        }
+        $takeoutHelper->getRestaurants($allData, $data);
+        $takeoutHelper->findFoots($data, $curlHelper);
     }
 
 }
