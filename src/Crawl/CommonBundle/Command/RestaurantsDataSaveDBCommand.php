07d411908d862ee626bd64e84ff6b188f8b442a8
marquis <511680713@qq.com>
2016-04-08 14:02:41 +0800

    抓取 有错误时，重新获取并存到数据库

--- src/Crawl/CommonBundle/Command/RestaurantsDataSaveDBCommand.php
+++ src/Crawl/CommonBundle/Command/RestaurantsDataSaveDBCommand.php
@@ -10,12 +10,18 @@ namespace Crawl\CommonBundle\Command;
 
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RestaurantsDataSaveDBCommand extends ContainerAwareCommand
 {
+
+    const ELEME_BASE_URL = 'https://www.ele.me/restapi/v4/restaurants';
+
     protected function configure()
     {
         $this
@@ -26,6 +32,46 @@ class RestaurantsDataSaveDBCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $place = $input->getArgument('place');
+        $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
+        $con = $em->getConnection();
+        $platform = $con->getDatabasePlatform();
+
+        $baseUrl = self::ELEME_BASE_URL;
+
+        $takeoutHelper = $this->getContainer()->get('crawl_common.helper.takeout');
+        $restaurantService = $this->getContainer()->get('crawl_common.service.restaurant');
+        try {
+            //Truncate Restaurant foods
+            $con->executeUpdate($platform->getTruncateTableSQL('crawl_restaurant_foods', true));
+            //Delete Restaurant
+            $em->createQuery('DELETE FROM CrawlCommonBundle:Restaurants')->execute();
+            $allData = $takeoutHelper->getAllData($baseUrl, $place);
+            $data = ['place' => $place];
+            $takeoutHelper->getRestaurants($allData, $data);
+            $restaurantService->saveRestaurants($data);
+
+            foreach ($data['restaurants'] as $k => $restaurant) {
+                $food = [];
+                $id = $restaurant['id'];
+                $takeoutHelper->findFootsByRestaurant($food, $id);
+                $restaurantService->saveRestaurantFoods($id, $food['restaurant']['foods']);
+            }
+        } catch (\Exception $e) {
+            $output->writeln('import restaurants again');
+            $kernel = $this->getContainer()->get('kernel');
+            $application = new Application($kernel);
+            $application->setAutoExit(false);
+
+            $input = new ArrayInput(array(
+                'command' => 'crawl:eleme:save',
+                'place' => 'webxpz39m7f'
+            ));
+
+            $outputNew = new BufferedOutput();
+            $application->run($input, $outputNew);
+        }
+
+        $output->writeln('import restaurants and foods success');
     }
 
 }
